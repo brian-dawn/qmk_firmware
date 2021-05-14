c3637f40a7d1915030db3e6a544777daaa0b5d71
Brian Dawn <brian.t.dawn@gmail.com>
2021-05-14 14:59:38 -0500

    Add changes for the quefrency 65%.

--- keyboards/keebio/quefrency/rev3/rules.mk
+++ keyboards/keebio/quefrency/rev3/rules.mk
@@ -1,4 +1,6 @@
 BOOTLOADER = atmel-dfu
 BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = no
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
